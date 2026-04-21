69f1e0728947579b0a80130058ae95fe1cd583d6
Ankit4399 <ankitkumar380780@gmail.com>
2026-04-22 02:28:48 +0530

    setup database with docker

--- src/common/config/db.js
+++ src/common/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose"
 
 
 const connectDB = async ()=>{
-    const conn = await mongoose.connect(process.env.MONGO_URI)
+    const conn = await mongoose.connect(process.env.MONGO_URI_DOCKER)
     
     console.log(`MongoDB Connected: ${conn.connection.host}`);
     
